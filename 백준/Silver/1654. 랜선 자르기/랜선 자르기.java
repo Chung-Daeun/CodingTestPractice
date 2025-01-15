1cef8380ef20ba3d9ee898cfbf124b1f71092c9a
ChungDaeun <108713683+Chung-Daeun@users.noreply.github.com>
2025-01-15 09:56:19 +0900

    [Silver II] Title: 랜선 자르기, Time: 156 ms, Memory: 17728 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,59 +1,59 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
 
-public class Main{
-    static List<Long> longList = new ArrayList<>();
+public class Main {
+
+    static long[] arr;
 
     public static void main(String[] args) {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         try {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            int k = Integer.parseInt(st.nextToken());
-            long n = Long.parseLong(st.nextToken());
+            final int T = Integer.parseInt(st.nextToken());
+            final int K = Integer.parseInt(st.nextToken());
+            arr = new long[T];
 
-            for(int i = 0; i < k; i++){
-                longList.add(Long.parseLong(br.readLine()));
+            long max = 0;
+            for (int i = 0; i < T; i++) {
+                arr[i] = Integer.parseInt(br.readLine());
+
+                if (max < arr[i]) {
+                    max = arr[i];
+                }
             }
+            br.close();
 
-            Collections.sort(longList, Comparator.reverseOrder());
-
-            bw.write(String.valueOf(binarySearch(n, 1L, longList.get(0), 0L)));
+            bw.write(String.valueOf(binarySearch(T, K, 1L, max)));
             bw.flush();
             bw.close();
-
-        } catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static long binarySearch(long num, long l, long r, long len){
+    static long binarySearch(int t, int k, long min, long max) {
 
-//        System.out.println("l: " + l + "\t r:" + r + "\tlen: " + len);
-        if(l > r){
-            return len;
+        if (min > max) {
+            return 0;
         }
 
-        long mid = (l + r) / 2;
+        long mid = (min + max) / 2;
+        int sum = 0;
 
-        long cnt = 0;
-
-        for(int i = 0; i < longList.size(); i++){
-            cnt += longList.get(i) / mid;
+        for (int i = 0; i < t; i++) {
+            sum += arr[i] / mid;
         }
 
-//        System.out.println("cnt: " + cnt);
-
-        if(cnt >= num){
-            if(len == 0 || len < mid){
-                return binarySearch(num, mid + 1, r, mid);
-            }
-            return binarySearch(num, mid + 1, r, len);
-        } else {
-            return binarySearch(num, l, mid - 1, len);
+        if (sum >= k) {
+            return Math.max(binarySearch(t, k, mid + 1, max), mid);
         }
+
+        return binarySearch(t, k, min, mid - 1);
+
     }
-
 }
