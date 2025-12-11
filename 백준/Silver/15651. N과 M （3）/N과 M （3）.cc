ea3980331628de0d4cf8b76c65887da336ea9049
ChungDaeun <108713683+Chung-Daeun@users.noreply.github.com>
2025-12-11 16:56:06 +0900

    [Silver III] Title: N과 M (3), Time: 304 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int N, M;
+vector<int> v(7);
+
+// 입력
+void input() {
+    // 입력
+    cin >> N >> M;
+}
+
+// 연산
+void rec_func(int k) {
+    if (k == M) {
+        // 다 골랐다 출력하자!
+        for (int i = 0; i < M; i++) {
+            cout << v[i] << " ";
+        }
+        cout << "\n";
+    } else {
+        // 골라보자
+        for (int i = 1; i <= N; i++) {
+            v[k] = i;
+            rec_func(k + 1);
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    input();
+    rec_func(0);
+    return 0;
+}
