25c9a8e66fc54169faa1e35a349b63eecc72036b
ChungDaeun <108713683+Chung-Daeun@users.noreply.github.com>
2026-02-06 22:36:13 +0900

    [Gold V] Title: 두 용액, Time: 8 ms, Memory: 2920 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.cc
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -1,76 +1,60 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 using namespace std;
 
-string text, word;
-
+// 변수
 int N;
-int arr[100005];
+vector<int> v;
 
 void input() {
     cin >> N;
+
+    int temp;
     for (int i = 0; i < N; i++) {
-        cin >> arr[i];
-    }
-}
-
-int lower_bound(int arr[], int left, int right, int target) {
-    int res = right + 1;
-    
-    while (left <= right) {
-        int mid = (left + right) / 2;
-        if (arr[mid] >= target) {
-            res = mid;
-            right = mid - 1;
-        } else {
-            left = mid + 1;
-        }
+        cin >> temp;
+        v.push_back(temp);
     }
 
-    return res;
+    // 정렬
+    sort(v.begin(), v.end());
+
 }
 
 void pro() {
-    sort(arr, arr + N);
+    int L = 0, R = N - 1, best_sum = 0x7fffffff;
+    int s1, s2;
 
-    int bestSum = 0x7fffffff;
-    int ans1 = 0;
-    int ans2 = 1;
+    while (L < R) {
+        int sum = v[L] + v[R];
 
-    for (int i = 0; i < N - 1; i++) {
-        int j = lower_bound(arr, i + 1, N - 1, -arr[i]);
-
-        // j - 1이랑 j 둘다 비교해야함.(인덱스 범위 넘어가지 않도록 주의)
-        if (i < j - 1 && abs(arr[i] + arr[j - 1]) < bestSum) {
-            bestSum = abs(arr[i] + arr[j - 1]);
-            ans1 = i;
-            ans2 = j - 1;
+        // 정답 갱신
+        if (abs(best_sum) > abs(sum)) {
+            s1 = v[L];
+            s2 = v[R];
+            best_sum = sum;
         }
 
-        if (j < N && abs(arr[i] + arr[j]) < bestSum) {
-            bestSum = abs(arr[i] + arr[j]);
-            ans1 = i;
-            ans2 = j;
-        }
-
-        
-
+        // L, R 이동
+        if (sum > 0)    R--;
+        if (sum < 0)    L++;
+        if (sum == 0)   break;
     }
 
-    cout << arr[ans1] << " " << arr[ans2];
-    
+    // 출력
+    cout << s1 << " " << s2;
 }
 
 int main() {
-    // 입출력 최적화
+    // 최적화
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
 
     // 입력
     input();
+
     // 실행
     pro();
-
 }
